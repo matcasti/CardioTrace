ef9b93cff09b04bce8de639360bc0c63368e8100
Matías Castillo <matcasti@umag.cl>
2026-07-15 21:04:06 -0300

    -

--- manuscript/raw_data/prep-data.R
+++ manuscript/raw_data/prep-data.R
@@ -15,9 +15,9 @@ rmse <- sqrt(mean((elitehrv-cardiotrace)^2))
 mape <- mean(abs((elitehrv-cardiotrace)/elitehrv)) * 100
 
 fig1a <- ggplot(data, aes(time)) +
-  geom_line(aes(y = elitehrv, col = "EliteHRV"), linewidth = 1) +
+  geom_line(aes(y = elitehrv, col = "EliteHRV"), linewidth = 2, alpha = 0.5) +
   geom_line(aes(y = cardiotrace, col = "CardioTrace")) +
-  scale_color_manual(values = c(EliteHRV = "orange", CardioTrace = "black")) +
+  scale_color_manual(values = c(EliteHRV = "gray", CardioTrace = "darkred")) +
   theme_classic() +
   labs(color = "Application", x = "Time (minutes)", y = "R-R interval (ms)") +
   theme(legend.position = "top") +
@@ -25,9 +25,9 @@ fig1a <- ggplot(data, aes(time)) +
   annotate(geom = "text", label = paste0("MAPE = ", round(mape,3), "%"), y = 850-15, x = 2.5, hjust = 0)
 
 fig1b <- ggplot(data, aes(elitehrv, cardiotrace, col = "Data point")) +
+  geom_abline(slope = 1, intercept = 0, alpha = 0.5, linewidth = 1) +
   geom_point() +
-  geom_abline(slope = 1, intercept = 0) +
-  scale_color_manual(values = c("Data point" = "orange")) +
+  scale_color_manual(values = c("Data point" = "darkred")) +
   theme_classic() +
   labs(color = "Symbol", x = "EliteHRV R-R interval (ms)", y = "CardioTrace R-R interval (ms)") +
   theme(legend.position = "top") +
@@ -35,5 +35,5 @@ fig1b <- ggplot(data, aes(elitehrv, cardiotrace, col = "Data point")) +
 
 fig1 <- cowplot::plot_grid(fig1a, fig1b, ncol = 2, axis = "lb", align = "hv")
 
-ggsave("manuscript/figure-2.png", fig1, device = "png", scale = 4, units = "px",
-       height = 400, width = 600)
+ggsave("manuscript/figure-2.png", fig1, device = "png", scale = 6, units = "px",
+       height = 400, width = 600, dpi = 400)
